1e47af22536a1aa19d712145e2b58e32a9d8e35c
Abhishek Venkat <abhisaivenkat@gmail.com>
2021-06-07 16:07:21 -0400

    add idle dialog

---
+++ client/src/components/TimeoutDialog.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: 'TimeoutDialog',
+  data: () => ({
+    show: false,
+  }),
+  watch: {
+    isAppIdle(idle) {
+      if (idle) {
+        this.show = true;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <v-dialog
+    v-model="show"
+    width="500"
+  >
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Warning
+      </v-card-title>
+
+      <v-card-text>
+        You have been inactive for more than 1 hour. Your session will
+        automatically terminate in 5 minutes
+      </v-card-text>
+
+      <v-divider />
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="primary"
+          text
+          @click="show = false"
+        >
+          I accept
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
--- client/src/views/Dataset.vue
+++ client/src/views/Dataset.vue
@@ -5,17 +5,19 @@ import {
   NavigationFailureType,
   isNavigationFailure,
 } from 'vue-router/src/util/errors';
-import Layout from '@/components/Layout.vue';
+
 import {
   mapState, mapActions, mapGetters, mapMutations,
 } from 'vuex';
 
+import Layout from '@/components/Layout.vue';
 import NavbarTitle from '@/components/NavbarTitle.vue';
 import UserButton from '@/components/girder/UserButton.vue';
 import SessionsView from '@/components/SessionsView.vue';
 import WindowControl from '@/components/WindowControl.vue';
 import ScreenshotDialog from '@/components/ScreenshotDialog.vue';
 import EmailDialog from '@/components/EmailDialog.vue';
+import TimeoutDialog from '@/components/TimeoutDialog.vue';
 import KeyboardShortcutDialog from '@/components/KeyboardShortcutDialog.vue';
 import NavigationTabs from '@/components/NavigationTabs.vue';
 import SessionTimer from '@/components/SessionTimer.vue';
@@ -33,6 +35,7 @@ export default {
     WindowControl,
     ScreenshotDialog,
     EmailDialog,
+    TimeoutDialog,
     KeyboardShortcutDialog,
     NavigationTabs,
     SessionTimer,
@@ -802,6 +805,7 @@ export default {
       v-model="emailDialog"
       :notes="notes"
     />
+    <TimeoutDialog />
     <KeyboardShortcutDialog v-model="keyboardShortcutDialog" />
   </v-layout>
 </template>
